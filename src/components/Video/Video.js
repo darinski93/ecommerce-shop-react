b39284533a5eaa1f3fd2761755fb56d6e0642f4c
Darios Petrov <darios.petrov93@gmail.com>
2023-08-05 12:00:52 +0300

    adding Video

---
+++ src/components/Video/Video.js
@@ -0,0 +1,9 @@
+export const Video = () => {
+
+    return (
+        <video autoPlay muted loop id="background-video">
+            <source src="./backVideo.mp4" type="video/mp4" />
+        </video>
+    )
+
+}
