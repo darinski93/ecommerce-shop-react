d58f92465d770c3d69331b9de3c00c54d3d4783e
Darios Petrov <darios.petrov93@gmail.com>
2023-08-06 20:27:36 +0300

    adding css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
 import { Video } from './components/Video/Video';
 import { Category } from './components/Category/Category';
+import { Bracelet } from './components/Bracelet/Bracelet';
+import { Item } from './components/Item/Item';
 
 
 function App() {
+
+  const location = useLocation()
+  const isHomePage = location.pathname === '/'
+
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/category' element={<Category />} />
+        <Route path='/category-bracelets' element={<Bracelet />} />
+        <Route path='/category/item/:itemId' element={<Item />} />
       </Routes>
 
       <Video />
-      <Footer />
+      {isHomePage && <Footer />}
     </div>
   );
 }
