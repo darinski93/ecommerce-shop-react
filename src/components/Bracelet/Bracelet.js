f0933e62e13dd2ed3c15604e6a84dac6d5944856
Darios Petrov <darios.petrov93@gmail.com>
2023-08-06 20:27:25 +0300

    add Bracelet, Item components

--- src/components/Bracelet/Bracelet.js
+++ src/components/Bracelet/Bracelet.js
@@ -1,14 +1,60 @@
+import { useEffect, useState } from "react"
+
+
 export const Bracelet = () => {
 
+    const [items, setItems] = useState([])
+    const [show, setShow] = useState(10)
+
+    useEffect(() => {
+        fetch('./db/db.bracelet.json')
+            .then((res) => res.json())
+            .then((data) => setItems(data))
+            .catch((error) => console.log(error))
+    }, [])
+
+    if (!items || items.length === 0) {
+        return <div className="loading">Loading...</div>
+    }
+
+    const handleLoadMore = () => {
+        setShow((prevShow) => prevShow + 10)
+    }
+
+    const handleShowLess = () => {
+        setShow(10)
+    }
 
     return (
         <div className="product-grid">
-            <div className="product-item">
-                <img src="https://www.customcuff.co/cdn/shop/products/Screenshot2021-11-02at15.40.37_800x.png?v=1635864229" alt="Product 1" />
-                <h2>Product 1</h2>
-                <p>Short description of Product 1</p>
+
+            <div className="sorting-container">
+                <button onClick={() => setItems([...items].sort((a, b) => a.name.localeCompare(b.name)))}>A-z</button>
+                <button onClick={() => setItems([...items].sort((a, b) => b.name.localeCompare(a.name)))}>Z-a</button>
+                <button onClick={() => setItems([...items].sort((a, b) => a.price - b.price))}>Price-low to high</button>
+                <button onClick={() => setItems([...items].sort((a, b) => b.price - a.price))}>Price-high to low</button>
             </div>
-            <button id="load-more-btn">Load More</button>
+
+            <div className="describing">
+                {items[0].category} {'\u003e'} {show > items.length ? items.length : show} Products
+            </div>
+
+            {items.slice(0, show).map((item, index) => (
+                <div className="product-item" key={index} style={{ backgroundImage: `url(${item.image})` }}>
+                    <div>{item.name}</div>
+                    <p>{item.description}</p>
+                    <h4 className="price">${item.price}</h4>
+                </div>
+            ))}
+
+            <div className="btn-container">
+                {show > items.length ? (
+                    <button id="load-more-btn" onClick={handleShowLess}>Show Less</button>
+                ) : (
+                    <button id="load-more-btn" onClick={handleLoadMore}>Load More</button>
+                )}
+            </div>
+
         </div>
     )
-}
+};
